b1ba3e910eeedef90ecc651cc456c63d9a93aff5
DovhanEduard <145812408+DovhanEduard@users.noreply.github.com>
2024-11-04 14:43:41 +0200

    fix utils

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants/index.js';
+import { env } from './env.js';
 
 export const saveFileToUploadDir = async (file) => {
   await fs.rename(
@@ -8,5 +9,5 @@ export const saveFileToUploadDir = async (file) => {
     path.join(UPLOAD_DIR, file.filename),
   );
 
-  return `uploads/${file.filename}`;
+  return `${env('APP_DOMAIN')}/uploads/${file.filename}`;
 };
