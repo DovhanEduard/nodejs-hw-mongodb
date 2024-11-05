7c9d4a2ea4d2cf4099a50ab87cde336f718efc14
DovhanEduard <145812408+DovhanEduard@users.noreply.github.com>
2024-11-05 13:28:48 +0200

    corect reset-pwd link

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { pinoHttp } from 'pino-http';
+// import { pinoHttp } from 'pino-http';
 import { env } from './utils/env.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
--- src/services/auth.js
+++ src/services/auth.js
@@ -112,7 +112,9 @@ export const requestResetToken = async (email) => {
     from: env(SMTP.SMTP_FROM),
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    html: `<p>Click <a href="${env(
+      'APP_DOMAIN',
+    )}/reset-password?token=${resetToken}">here</a> to reset your password!</p>`,
   });
 };
 
